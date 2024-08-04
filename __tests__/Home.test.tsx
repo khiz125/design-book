edc56d48a8e923d95831a4f70d5d16df5f8f4da2
khiz125 <ken.hiro118@gmail.com>
2024-08-04 16:01:37 +0900

    Versioning untracked files

--- __tests__/Home.test.tsx
+++ __tests__/Home.test.tsx
@@ -1,5 +1,6 @@
+import React from 'react'
 import { render, screen } from "@testing-library/react";
-import Home from "@/app/page";
+import Home from "@/src/app/page.tsx";
 
 it("should have Docs text", () => {
   render(<Home />);

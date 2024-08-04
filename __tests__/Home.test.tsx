97bfe0f1b86510b84880affb2b3a34532232fac9
khiz125 <ken.hiro118@gmail.com>
2024-08-04 16:18:37 +0900

    fix tsconfig

--- __tests__/Home.test.tsx
+++ __tests__/Home.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { render, screen } from "@testing-library/react";
-import Home from "@/src/app/page.tsx";
+import Home from "@/app/page";
 
 it("should have Docs text", () => {
   render(<Home />);

8adefd045d65da5ee4693f837414bb9181b8e2ff
khiz125 <ken.hiro118@gmail.com>
2024-08-17 15:48:13 +0900

    fix style

--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -4,18 +4,21 @@ import React from 'react'
 
 const Sidebar = () => {
   return (
-    <nav className='bg-white border-r border-gray-400 w-14 p-2'>
+    <nav className='bg-white border-r border-gray-400 lg:w-48 w-16'>
       <section className='flex flex-col w-full mt-10'>
-        <Link href="/" className='w-full flex items-center'>
-          <figure className='m-auto'>
-            <Image
-              src="/images/icons/book_blue.png"
-              alt="Picture of book"
-              width={30}
-              height={30}
-              priority
-            />
-          </figure>
+        <Link href="/" className='w-full hover:bg-slate-100 rounded h-12'>
+          <div className='flex items-center w-full lg:m-4 m-2 gap-2'>
+            <figure className='p-2'>
+              <Image
+                src="/images/icons/book_blue.png"
+                alt="Picture of book"
+                width={28}
+                height={28}
+                priority
+              />
+            </figure>
+            <p className='hidden lg:contents'>Home</p>
+          </div>
         </Link>
       </section>
     </nav>

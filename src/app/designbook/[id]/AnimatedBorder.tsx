781141ec3b4df40d77f52a2300d705c8bf1dbebe
khiz125 <ken.hiro118@gmail.com>
2024-08-18 16:06:40 +0900

    feat:add animated border page

--- src/app/designbook/[id]/AnimatedBorder.tsx
+++ src/app/designbook/[id]/AnimatedBorder.tsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 const AnimatedBorder = () => {
   return (
-    <div className='flex justify-center items-center bg-[#4682B4] w-full h-full'>
-      AnimatedBorder
+    <div className='flex flex-col justify-center items-center bg-slate-950 text-white w-full h-full'>
+      <div className='card-wrapper h-[200px] w-[200px]'>
+        <div className='card-content flex items-center justify-center text-xs'>Animated Border</div>
+      </div>
     </div>
   )
 }

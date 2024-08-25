0826eabe0c7215aeaa23d0d52a609bd953486877
khiz125 <ken.hiro118@gmail.com>
2024-08-25 22:18:22 +0900

    add display: 'swap' in fonts.ts

--- src/styles/fonts.ts
+++ src/styles/fonts.ts
@@ -1,5 +1,5 @@
 import {
-  Alegreya,
+	Alegreya,
 	Anton,
 	Asset,
 	Dancing_Script,
@@ -15,107 +15,124 @@ import {
 	Mystery_Quest,
 	New_Tegomin,
 	Ruluko,
-  Stalemate,
+	Stalemate,
 } from "next/font/google";
 
 export const inter = Inter({
 	subsets: ["latin"],
 	variable: "--font-inter",
-  weight: '400',
+	weight: "400",
+	display: "swap",
 });
 
 export const lora = Lora({
 	subsets: ["latin"],
 	variable: "--font-lora",
-  weight: '400',
+	weight: "400",
+	display: "swap",
 });
 
 export const newTegomin = New_Tegomin({
 	subsets: ["latin"],
 	variable: "--font-new-tegomin",
 	weight: "400",
+	display: "swap",
 });
 
 export const ruluko = Ruluko({
 	subsets: ["latin"],
 	variable: "--font-ruluko",
 	weight: "400",
+	display: "swap",
 });
 
 export const gentiumBookPlus = Gentium_Book_Plus({
 	subsets: ["latin"],
 	variable: "--font-gentium-book-plus",
 	weight: "400",
+	display: "swap",
 });
 
 export const asset = Asset({
 	subsets: ["latin"],
 	variable: "--font-asset",
 	weight: "400",
+	display: "swap",
 });
 
 export const mysteryQuest = Mystery_Quest({
 	subsets: ["latin"],
 	variable: "--font-mystery-quest",
 	weight: "400",
+	display: "swap",
 });
 
 export const stalemate = Stalemate({
-  subsets: ['latin'],
-  variable: '--font-stalemate',
-  weight: '400',
+	subsets: ["latin"],
+	variable: "--font-stalemate",
+	weight: "400",
+	display: "swap",
 });
 
 export const fontdinerSwanky = Fontdiner_Swanky({
-  subsets: ['latin'],
-  variable: '--font-fontdiner-swanky',
-  weight: '400',
+	subsets: ["latin"],
+	variable: "--font-fontdiner-swanky",
+	weight: "400",
+	display: "swap",
 });
 
 export const mcLaren = McLaren({
-  subsets: ['latin'],
-  variable: '--font-mclaren',
-  weight: '400',
+	subsets: ["latin"],
+	variable: "--font-mclaren",
+	weight: "400",
+	display: "swap",
 });
 
 export const imFellEnglishSC = IM_Fell_English_SC({
-  subsets: ['latin'],
-  variable: '--font-im-fell-english-sc',
-  weight: '400',
+	subsets: ["latin"],
+	variable: "--font-im-fell-english-sc",
+	weight: "400",
+	display: "swap",
 });
 
 export const libreFranklin = Libre_Franklin({
-  subsets: ['latin'],
-  variable: '--font-libre-franklin',
-  weight: ['100', '300', '400', '600', '700', '800', '900'],
+	subsets: ["latin"],
+	variable: "--font-libre-franklin",
+	weight: ["100", "300", "400", "600", "700", "800", "900"],
+	display: "swap",
 });
 
 export const merriweatherSans = Merriweather_Sans({
-  subsets: ['latin'],
-  variable: '--font-merriweather-sans',
-  weight: ['300', '400', '500', '600', '700', '800'],
+	subsets: ["latin"],
+	variable: "--font-merriweather-sans",
+	weight: ["300", "400", "500", "600", "700", "800"],
+	display: "swap",
 });
 
 export const alegreya = Alegreya({
-  subsets: ['latin'],
-  variable: '--font-alegreya',
-  weight: ['400', '700', '900'],
+	subsets: ["latin"],
+	variable: "--font-alegreya",
+	weight: ["400", "700", "900"],
+	display: "swap",
 });
 
 export const dancingScript = Dancing_Script({
-  subsets: ['latin'],
-  variable: '--font-dancing-script',
-  weight: ['400', '500', '600', '700'],
+	subsets: ["latin"],
+	variable: "--font-dancing-script",
+	weight: ["400", "500", "600", "700"],
+	display: "swap",
 });
 
 export const anton = Anton({
-  subsets: ['latin'],
-  variable: '--font-anton',
-  weight: '400',
+	subsets: ["latin"],
+	variable: "--font-anton",
+	weight: "400",
+	display: "swap",
 });
 
 export const francoisOne = Francois_One({
-  subsets: ['latin'],
-  variable: '--font-francois-one',
-  weight: '400',
+	subsets: ["latin"],
+	variable: "--font-francois-one",
+	weight: "400",
+	display: "swap",
 });

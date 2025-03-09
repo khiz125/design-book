4a54da8a9b3b50c278ba4796e46b83aab90dea11
khiz125 <ken.hiro118@gmail.com>
2025-03-09 22:45:30 +0900

    Revert "refactor"
    This reverts commit f4e290f0448afcd1c01f922dd967398317107cc1.

--- src/app/components/TimeLine.tsx
+++ src/app/components/TimeLine.tsx
@@ -14,91 +14,71 @@ const TimeLine = () => {
   const [parentWidth, setParentWidth] = useState(0);
   const genTimeLineItems = useMemo<Pokemon[]>(() => {
     const numbers = Array.from({ length: 12 }, (_, index) => index + 1);
-    return numbers.map((n) => POKEMON_DATA[n]).filter((pokemon) => pokemon);
+    return numbers.map(n => POKEMON_DATA[n]).filter(pokemon => pokemon);
   }, []);
   const [cards, setCards] = useState<Pokemon[]>(genTimeLineItems);
   const ref = useRef<HTMLDivElement | null>(null);
   const cardRefs = useRef<(HTMLDivElement | null)[]>([]);
-  const [visible, setVisible] = useState<boolean[]>(
-    Array.from({ length: cards.length }, () => false),
-  );
-  const generateNewCards = useMemo(() => {
-    return (count: number) => {
-      return Array.from({ length: count }, (_, i) => ({
-        ...genTimeLineItems[i % genTimeLineItems.length],
-      }));
-    };
-  }, [genTimeLineItems]);
+  const [visible, setVisible] = useState<boolean[]>(Array.from({ length: cards.length }, () => false));
+  const generateNewCards = (count: number): Pokemon[] => {
+    return Array.from({ length: count }, (_, i) => ({
+      ...genTimeLineItems[i % genTimeLineItems.length]
+    }))
+  };
 
   const setRef = (index: number) => (el: HTMLDivElement | null) => {
     cardRefs.current[index] = el;
-  };
+  }
   useEffect(() => {
-    if (typeof window !== "undefined" && "IntersectionObserver" in window) {
-      const observer = new IntersectionObserver(
-        (entries) => {
-          entries.forEach((entry) => {
-            const index = cardRefs.current.indexOf(
-              entry.target as HTMLDivElement,
-            );
-            if (index !== -1) {
-              if (entry.isIntersecting) {
-                setVisible((prev) => {
-                  const newVisible = [...prev];
-                  newVisible[index] = true;
-                  return newVisible;
-                });
-                if (index === cards.length - 1) {
-                  setCards((prev) => [...prev, ...generateNewCards(10)]);
-                }
-              } else {
-                setVisible((prev) => {
-                  const newVisible = [...prev];
-                  newVisible[index] = false;
-                  return newVisible;
-                });
+    if (typeof window !== 'undefined' && 'IntersectionObserver' in window) {
+      const observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+          const index = cardRefs.current.indexOf(entry.target as HTMLDivElement);
+          if (index !== -1) {
+            if (entry.isIntersecting) {
+              setVisible(prev => {
+                const newVisible = [...prev];
+                newVisible[index] = true;
+                return newVisible;
+              })
+              if (index === cards.length - 1) {
+                setCards(prev => [...prev, ...generateNewCards(10)])
               }
+            } else {
+              setVisible(prev => {
+                const newVisible = [...prev];
+                newVisible[index] = false;
+                return newVisible;
+              });
             }
-          });
-        },
-        { threshold: 0.5 },
-      );
-
-      const currentRefs = cardRefs.current.slice();
-      currentRefs.forEach((ref) => {
+          }
+        });
+      }, { threshold: 0.5 });
+      cardRefs.current.forEach((ref) => {
         if (ref) {
-          observer.unobserve(ref);
+          observer.observe(ref);
         }
       });
       return () => {
-        currentRefs.forEach((ref) => {
+        cardRefs.current.forEach((ref) => {
           if (ref) {
             observer.unobserve(ref);
           }
-        });
-      };
+        })
+      }
     }
-  }, [cards, genTimeLineItems, generateNewCards]);
+  }, [cards]);
   useLayoutEffect(() => {
     if (ref.current) {
       setParentWidth(ref.current.offsetWidth);
     }
   }, []);
   return (
-    <div
-      ref={ref}
-      className={`w-full h-full flex flex-col justify-center bg-black"`}
-    >
-      <div
-        className={`${parentWidth < 310 ? "h-72" : "h-2/3"} border rounded bg-white m-4 overflow-scroll hidden-scrollbar`}
-      >
+    <div ref={ref} className="w-full h-full flex flex-col justify-center bg-black">
+      <div className={`${parentWidth < 310 ? "h-72" : "h-2/3"} border rounded bg-white m-4 overflow-scroll hidden-scrollbar`}>
         {cards.map((pokemon, i) => {
           return (
-            <div
-              key={i}
-              ref={setRef(i)}
-              className={`w - full px - 14  ${visible[i] ? "animate-slideIn" : "animate-slideOut"} `}
-            >
+            <div key={i} ref={setRef(i)} className={`w-full px-14  ${visible[i] ? 'animate-slideIn' : 'animate-slideOut'}`}>
               <MessageBox parentWidth={parentWidth} pokemon={pokemon} />
             </div>
           );
@@ -118,11 +98,11 @@ const MessageBox: React.FC<MessageBoxProps> = ({ parentWidth, pokemon }) => {
   const fonts = FONTS;
   return (
     <article
-      className={`flex mt - 2 ${parentWidth < 310 ? "h-10" : "h-[120px]"} justify - center border border - gray - 300 rounded ${fonts[11].font.className} font - ${fonts[11].name} `}
+      className={`flex mt-2 ${parentWidth < 310 ? "h-10" : "h-[120px]"} justify-center border border-gray-300 rounded ${fonts[11].font.className} font-${fonts[11].name}`}
     >
-      <div className={`${parentWidth < 310 ? "" : "my-4"} flex mx - 2`}>
+      <div className={`${parentWidth < 310 ? "" : "my-4"} flex mx-2`}>
         <figure
-          className={`${parentWidth < 310 ? "text-[10px] transform scale-50" : "max-w-1 mx-10 my-2"} `}
+          className={`${parentWidth < 310 ? "text-[10px] transform scale-50" : "max-w-1 mx-10 my-2"}`}
         >
           <Image
             src={pokemon.image}
@@ -132,19 +112,19 @@ const MessageBox: React.FC<MessageBoxProps> = ({ parentWidth, pokemon }) => {
           />
         </figure>
         <div
-          className={`w-full flex flex-col ${parentWidth ? "h-full" : "h-[120px] mx-2"} `}
+          className={`w-full flex flex-col ${parentWidth ? "h-full" : "h-[120px] mx-2"}`}
         >
-          <div
-            className={`flex items - center w - full ${parentWidth ? "h-full" : "h-[120px] mx-2"} `}
-          >
+          <div className={`flex items-center w-full ${parentWidth ? "h-full" : "h-[120px] mx-2"}`}>
             <p
-              className={`${parentWidth < 310 ? "text-[10px]" : "w-36 font-bold text-xl text-gray-600"} `}
+              className={`${parentWidth < 310 ? "text-[10px]" : "w-36 font-bold text-xl text-gray-600"}`}
             >
               {pokemon.name}
             </p>
           </div>
           <div className="w-full">
-            <p>{parentWidth > 310 && pokemon.description}</p>{" "}
+            <p>
+              {parentWidth > 310 && pokemon.description}
+            </p>{" "}
           </div>
         </div>
       </div>

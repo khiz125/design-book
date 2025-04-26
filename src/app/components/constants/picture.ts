ab4fb2a035913e03e0de691d710103af8f563856
khiz125 <ken.hiro118@gmail.com>
2025-04-26 20:58:20 +0900

    add back to top scroll button

---
+++ src/app/components/constants/picture.ts
@@ -0,0 +1,47 @@
+type Pictures = {
+  name: string;
+  images: string[];
+};
+
+export const PICTURES: Pictures[] = [
+  {
+    name: "Nature",
+    images: [
+      "https://photosku.com/photo/images/s/p00/00386.jpg",
+      "https://photosku.com/photo/images/s/p00/00385.jpg",
+      "https://photosku.com/photo/images/s/p06/06419.jpg",
+    ],
+  },
+  {
+    name: "Animal",
+    images: [
+      "https://photosku.com/photo/images/s/p02/02702.jpg",
+      "https://photosku.com/photo/images/s/p05/05741.jpg",
+      "https://photosku.com/photo/images/s/p03/03223.jpg",
+    ],
+  },
+  {
+    name: "Town",
+    images: [
+      "https://photosku.com/photo/images/s/p06/06654.jpg",
+      "https://photosku.com/photo/images/s/p00/00655.jpg",
+      "https://photosku.com/photo/images/s/p06/06635.jpg",
+    ],
+  },
+  {
+    name: "Food",
+    images: [
+      "https://photosku.com/photo/images/s/p04/04709.jpg",
+      "https://photosku.com/photo/images/s/p06/06576.jpg",
+      "https://photosku.com/photo/images/s/p05/05293.jpg",
+    ],
+  },
+  {
+    name: "Interior",
+    images: [
+      "https://photosku.com/photo/images/s/p06/06714.jpg",
+      "https://photosku.com/photo/images/s/p06/06571.jpg",
+      "https://photosku.com/photo/images/s/p00/00244.jpg",
+    ],
+  },
+];

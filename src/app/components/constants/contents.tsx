7e03f4c691ce6e5e234c856cf8fc380e3e376cfc
khiz125 <ken.hiro118@gmail.com>
2025-06-01 15:13:25 +0900

    add link to my another project

--- src/app/components/constants/contents.tsx
+++ src/app/components/constants/contents.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import InvitationCard from "@/app/designbook/[id]/InvitationCard";
 import AnimatedBorder from "@/app/designbook/[id]/AnimatedBorder";
 import SplitComplimentTriad from "@/app/designbook/[id]/SplitComplimentTriad";
-import UnderConstruction from '@/app/designbook/[id]/UnderConstruction';
 import InfinityLoop from '@/app/designbook/[id]/InfinityLoop';
 import SmoothScroll from '@/app/designbook/[id]/SmoothScroll';
 
@@ -18,5 +17,4 @@ export const PAGE_CONTENTS:PageContents[] = [
 	{ id: "using-colors", title: "Split complement triad", component: <SplitComplimentTriad /> },
   { id: "loop-slider", title: "Loop Slider", component: <InfinityLoop /> },
   { id: "smooth-scroll", title: "Smooth scrolling", component: <SmoothScroll /> },
-  { id: "under-construction", title: "Under Construction...", component: <UnderConstruction /> },
 ];
---
+++ src/app/designbook/[id]/SentimentAnalyzer.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const SentimentAnalyzer = () => {
+  return (
+    <div className="hover:scale-110 m-0 hover:duration-200 overflow-hidden w-[300px] h-[400px] shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)]">
+      <a
+        href="https://nlp-app-xi.vercel.app/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <section className="w-full h-full bg-slate-200 flex items-center justify-center">
+          <div className="bg-white px-9 py-20 rounded-full">
+            あさりの研究室
+          </div>
+        </section>
+      </a>
+    </div>
+  );
+};
+
+export default SentimentAnalyzer;
